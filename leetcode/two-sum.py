1c39d0855df764c455548209a0f990d326985973
sharingov <dauletkhan.temirgaliyev@nu.edu.kz>
2021-05-10 16:53:50 +0600

    bim

--- leetcode/two-sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:    
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = dict()
-        for i in range(len(nums)):
-            val = d.get(target - nums[i])
-            if val:
-                return [i, val]
-            d[nums[i]] = i
